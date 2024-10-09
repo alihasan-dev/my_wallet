68a7a0238fa40aac0dd804fc6c5544e4c4ffbb36
Ali Hasan <alihasan.dev@gmail.com>
2024-10-10 04:09:23 +0530

    Update Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,15 @@ import '../utils/preferences.dart';
 import 'features/my_app/presentation/my_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAdGnZegndZF2qOWwWA_RPrzC9iQvrydPc',
+      appId: '1:976324609510:android:62ceacf3c246006de4c227',
+      messagingSenderId: '976324609510',
+      projectId: 'my-wallet-99fdf',
+      storageBucket: 'my-wallet-99fdf.appspot.com'
+    )
+  );
   await Preferences().init();
   runApp(BlocProvider(create: (_) => MyAppBloc(), child: const MyApp()));  
 }
