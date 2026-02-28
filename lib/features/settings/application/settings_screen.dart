38e85c9c70e09cab88b1732386edeb286f35b2d2
Ali Hasan <alihasan.dev@gmail.com>
2026-02-28 12:49:19 +0530

    Enable currency picker

--- lib/features/settings/application/settings_screen.dart
+++ lib/features/settings/application/settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sample_formatter/sample_formatter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../../l10n/app_localizations.dart';
 import '../../../features/settings/application/bloc/settings_bloc.dart';
@@ -21,7 +22,6 @@ import '../../../constants/app_strings.dart';
 import '../../../utils/preferences.dart';
 import '../../../widgets/custom_text.dart';
 import '../../../constants/app_size.dart';
-// import 'package:sample_formatter/sample_formatter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -36,13 +36,13 @@ class _SettingsScreenState extends State<SettingsScreen> {
   var languageList = <SettingLanguageModel>[];
   AppLocalizations? _localizations;
   late SettingsBloc _settingBloc;
-  // CurrencyModel? currencyModel;
+  CurrencyModel? currencyModel;
 
-  // @override
-  // void initState() {
-  //   currencyModel = CurrencyModel(countryCode: "IN");
-  //   super.initState();
-  // }
+  @override
+  void initState() {
+    currencyModel = CurrencyModel(countryCode: "IN");
+    super.initState();
+  }
 
   @override
   void didChangeDependencies() {
@@ -64,7 +64,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       settingItemList.add(SettingModel(icon: AppIcons.fingerprintIcon, title: _localizations!.enableBiometric, subTitle: _localizations!.enableBiometricMsg, showSwitch: true));
       settingItemList.add(SettingModel(icon: AppIcons.adsClickIcon, title: _localizations!.openAppOnBrowser, subTitle: AppStrings.webUrl, isLauncher: true));
     }
-    // settingItemList.add(SettingModel(icon: AppIcons.currencyIcon, title: "Currency"));
+    settingItemList.add(SettingModel(icon: AppIcons.currencyIcon, title: "Currency"));
     settingItemList.add(SettingModel(icon: AppIcons.infoIcon, title: _localizations!.aboutMyWallet));
     super.didChangeDependencies();
   }
@@ -192,10 +192,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
           break;
         case 4:
         case 6:
-        //   showCurrencyDialog(context: context);
-        //   break;
-        // case 5:
-        // case 7:
+          showCurrencyDialog(context: context);
+          break;
+        case 5:
+        case 7:
           showAboutAppDialog(context: context);
           break;
         default:
@@ -235,21 +235,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 
-  // void showCurrencyDialog({required BuildContext context}) {
-  //   showGeneralDialog(
-  //     context: context, 
-  //     barrierDismissible: true,
-  //     barrierLabel: AppStrings.close,
-  //     pageBuilder: (_, a1, _) => CurrencyDialogView(
-  //       selectedCurrency: currencyModel,
-  //       onSelect: (p0) {
-  //         currencyModel = p0;
-  //         setState(() {});
-  //         context.pop();
-  //       },
-  //     )
-  //   );
-  // }
+  void showCurrencyDialog({required BuildContext context}) {
+    showGeneralDialog(
+      context: context, 
+      barrierDismissible: true,
+      barrierLabel: AppStrings.close,
+      pageBuilder: (_, a1, _) => CurrencyDialogView(
+        selectedCurrency: currencyModel,
+        onSelect: (p0) {
+          currencyModel = p0;
+          setState(() {});
+          context.pop();
+        },
+      )
+    );
+  }
 
   void showThemeDialog({required BuildContext context}) {
     showDialog(
